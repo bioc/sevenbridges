b76c33e7279f6f062c8d6638e2dc16f7f306cf24
lshep <lori.shepherd@roswellpark.org>
2026-09-14 10:41:37 -0400

    unresponsive
     - secret found and bounced email

--- R/zzz.R
+++ R/zzz.R
@@ -9,3 +9,9 @@
 
   options(sevenbridges = lst)
 }
+.onAttach <- function(libname, pkgname) {
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.25")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
+}
